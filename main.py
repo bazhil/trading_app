0f666a35b06e2d15e78637879e62c6985cf2af54
bazhil <i-bazh@mail.ru>
2023-04-10 02:29:27 +0300

    Learn validation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from typing import List
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
 from pydantic import BaseModel, Field
 
 from fastapi import FastAPI
@@ -8,16 +10,34 @@ app = FastAPI(
     title='Trading app'
 )
 
+
+
+
+fake_users = [
+    {'id': 1, 'name': 'Bob', 'degree': [
+        {'id': 1, 'created_at': '2020-01-01T00:00:00', 'type_degree': 'expert'}
+    ]},
+    {'id': 2, 'name': 'John'}
+]
+
+
+class DegreeType(Enum):
+    newbie = 'newbie'
+    expert = 'expert'
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+
 class User(BaseModel):
     id: int
     role: str
     name: str
+    degree: Optional[List[Degree]] = []
 
-
-fake_users = [
-    {'id': 1, 'name': 'Bob'},
-    {'id': 2, 'name': 'John'}
-]
 
 @app.get('/users/{id}', response_model=List[User])
 def get_user(id: int):
