42d4de9b3135e1cf418862546eaeab40c83907e6
bazhil <i-bazh@mail.ru>
2023-04-12 01:20:48 +0300

    Add env for migrations. Need create tables and migrations.

--- models/models.py
+++ models/models.py
@@ -7,7 +7,7 @@ metadata = MetaData()
 roles = Table(
     'roles',
     metadata,
-
+    Column('id', Integer, primary_key=True),
     Column('name', String, nullable=False),
     Column('permissions', JSON)
 )
