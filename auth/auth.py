b86eb7e310b93256a5d08e744254558c7851b3fc
bazhil <i-bazh@mail.ru>
2023-04-20 20:32:12 +0300

    Add auth settings

--- auth/auth.py
+++ auth/auth.py
@@ -1,4 +1,14 @@
-from fastapi_users.authentication import CookieTransport
+from fastapi_users.authentication import CookieTransport, JWTStrategy, AuthenticationBackend
 
-cookie_transport = CookieTransport(cookie_max_age=3600)
+from config import SECRET
 
+cookie_transport = CookieTransport(cookie_name='bonds', cookie_max_age=3600)
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy,
+)
