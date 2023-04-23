6e816d03ea6b97803b48f799649c18cc90f6257c
bazhil <i-bazh@mail.ru>
2023-04-24 02:34:17 +0300

    Add routes. Need fix errors.

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 from datetime import datetime
 from enum import Enum
 from typing import List, Optional
+
+from fastapi_users import fastapi_users, FastAPIUsers
 from pydantic import BaseModel, Field
 
 from fastapi import FastAPI
 
+from auth.auth import auth_backend
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
+from auth.database import User
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
 
 app = FastAPI(
     title='Trading app'
 )
 
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
 
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
 
 
 fake_users = [
