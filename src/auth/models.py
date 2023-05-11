9a23cc9bab62ed276f3a908e21aacd18e6132295
bazhil <i-bazh@mail.ru>
2023-05-12 02:04:09 +0300

    finish part 6 of lessons cycle

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, Boolean
 
 metadata = MetaData()
@@ -25,3 +26,17 @@ user = Table(
     Column("is_superuser", Boolean, default=False, nullable=False),
     Column("is_verified", Boolean, default=False, nullable=False),
 )
+
+# нужно для fastapi_users!
+
+# from src.database import Base
+# class User(SQLAlchemyBaseUserTable[int], Base):
+#     id = Column(Integer, primary_key=True)
+#     email = Column(String, nullable=False)
+#     username = Column(String, nullable=False)
+#     registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+#     role_id = Column(Integer, ForeignKey(role.c.id))
+#     hashed_password: str = Column(String(length=1024), nullable=False)
+#     is_active: bool = Column(Boolean, default=True, nullable=False)
+#     is_superuser: bool = Column(Boolean, default=False, nullable=False)
+#     is_verified: bool = Column(Boolean, default=False, nullable=False)
--- src/operations/router.py
+++ src/operations/router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
+from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import get_async_session
 from src.operations.models import operation
+from src.operations.schemas import OperationCreate
 
 router = APIRouter(
     prefix='/operations',
@@ -19,4 +20,8 @@ async def get_specific_operations(operation_type: str, session: AsyncSession = D
 
 @router.post('/')
 async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+    statement = insert(operation).values(**new_operation.dict())
+    await session.execute(statement)
+    await session.commit()
+
     return {'status': 'success'}
